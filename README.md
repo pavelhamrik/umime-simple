0dd06cd316ee7c253c685a907fbe5974a6a901af
Pavel Hamrik <pavel.hamrik@gmail.com>
2018-08-14 16:50:20 +0200

    basic gulp config

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # umime-simple
+
+simplified boilerplate for the front ends of the **Umime to** educational systems
+
+## dependencies
+
+- `gulp`: task automation
+- `gulp-sass`: scss compiling
+- `gulp-sourcemaps`: sourcemaps for css
+
+## todos
+
+- `foundation-sites`: only using a small subset, such as grids
+- `gulp-minify`: minify the css when building for production
+
+## consider
+
+- some `js` and `html` stage depending on legacy code integration
+- `browser-sync`: live reload
+
+## done
+
+- copying of assets such as images and fonts
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+const dist = './docs';
+const paths = {
+    assets: {
+        src: './src/assets/*',
+        dest: dist + '/assets/'
+    },
+    styles: {
+        src: './src/**/*.scss',
+        dest: dist + '/styles/'
+    }
+};
+
+gulp.task('default', ['styles', 'copy', 'watch']);
+
+// todo: build task minify etc
+// gulp.task('build', ['styles']);
+
+gulp.task('styles', function () {
+    return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.styles.dest));
+});
+
+gulp.task('copy', function () {
+    return gulp.src(paths.assets.src)
+        .pipe(gulp.dest(paths.assets.dest));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.styles.src, ['styles']);
+    gulp.watch(paths.assets.src, ['copy']);
+});
