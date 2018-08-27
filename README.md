3186c9b3918f53b3bfbbf341c584986f1beca977
Pavel Hamrik <pavel.hamrik@gmail.com>
2018-08-27 13:46:51 +0200

    styles update

--- README.md
+++ README.md
@@ -8,18 +8,16 @@ simplified boilerplate for the front ends of the [Umime to](https://www.umimeto.
 - `gulp-sass`: scss compiling
 - `gulp-sourcemaps`: sourcemaps for css
 - `foundation-sites`: the actual styling; only using a small subset, such as grids
+- `browser-sync`: live reload
+- `panini`: html templating; dev only
 
 ## todos
 
 - `gulp-minify`: minify the css when building for production
+- [x] copying of assets such as images and fonts
 
 ## consider
 
 - [ ] some `js` and `html` stage depending on legacy code integration
-- [ ] `browser-sync`: live reload
 - [ ] `gulp-imagemin`: image optimization
 - [ ] image optimization, generating multiple sizes
-
-## done
-
-- [x] copying of assets such as images and fonts
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const browser = require("browser-sync").create();
+const panini = require("panini");
+
 
 const dist = './docs';
+const port = '8008';
 const paths = {
     assets: {
         src: './src/assets/**/*',
-        dest: dist + '/assets/'
+        dist: dist + '/assets/'
     },
     styles: {
         src: './src/**/*.scss',
-        dest: dist + '/assets/'
+        dist: dist + '/assets/'
     },
-    sass: ['./node_modules/foundation-sites/scss']
+    pages: {
+        src: './src/pages/',
+        dist: dist
+    },
+    sass: [
+        './node_modules/foundation-sites/scss'
+    ]
 };
 
-gulp.task('default', ['styles', 'copy', 'watch']);
+gulp.task('default', ['pages', 'styles', 'copy', 'browser', 'watch']);
 
 // todo: build task minify etc
 // gulp.task('build', ['styles', 'copy']);
@@ -27,15 +37,45 @@ gulp.task('styles', function () {
             includePaths: paths.sass
         }).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.styles.dest));
+        .pipe(gulp.dest(paths.styles.dist))
+        .pipe(browser.stream());
+});
+
+gulp.task('pages', function () {
+    return gulp.src(paths.pages.src + 'pages/**/*.{html,hbs,handlebars}')
+        .pipe(panini({
+            root: paths.pages.src + 'pages/',
+            layouts: paths.pages.src + 'layouts/',
+            partials: paths.pages.src + 'partials/',
+            data: paths.pages.src + 'data/',
+            helpers: paths.pages.src + 'helpers/'
+        }))
+        .pipe(gulp.dest(paths.pages.dist));
+});
+
+gulp.task('pages-refresh', function(done) {
+    panini.refresh();
+    done();
 });
 
 gulp.task('copy', function () {
     return gulp.src(paths.assets.src)
-        .pipe(gulp.dest(paths.assets.dest));
+        .pipe(gulp.dest(paths.assets.dist));
+});
+
+gulp.task('browser', function() {
+    browser.init({
+        server: dist,
+        port: port
+    });
+});
+
+gulp.task('browser-reload', function() {
+    browser.reload();
 });
 
 gulp.task('watch', function () {
     gulp.watch(paths.styles.src, ['styles']);
-    gulp.watch(paths.assets.src, ['copy']);
+    gulp.watch(paths.assets.src, ['copy', 'browser-reload']);
+    gulp.watch(paths.pages.src + '**/*', ['pages-refresh', 'browser-reload']);
 });
