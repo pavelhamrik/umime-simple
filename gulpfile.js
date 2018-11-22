cd37ff0c3948f5d222c296e78992382f44ff6db6
Pavel Hamrik <pavel.hamrik@gmail.com>
2018-11-22 11:40:23 +0100

    transpiler

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,20 @@ const WEBPACK_CONFIG_DEV = {
 
 const WEBPACK_CONFIG_PROD = {
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                // exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ]
+    }
 };
 
 
